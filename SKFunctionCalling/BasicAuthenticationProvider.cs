fc8a33538fffb74f19ccc38d81374ab15cb83ed6
juan manuel boschetti <JuanManuelBoschetti@hotmail.com>
2024-01-08 15:49:10 -0300

    add function calling example

---
+++ SKFunctionCalling/BasicAuthenticationProvider.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKFunctionCalling
+{
+    /// <summary>
+    /// Retrieves authentication content (e.g. username/password, API key) via the provided delegate and
+    /// applies it to HTTP requests using the "basic" authentication scheme.
+    /// </summary>
+    public class BasicAuthenticationProvider
+    {
+        private readonly Func<Task<string>> _credentials;
+
+        /// <summary>
+        /// Creates an instance of the <see cref="BasicAuthenticationProvider"/> class.
+        /// </summary>
+        /// <param name="credentials">Delegate for retrieving credentials.</param>
+        public BasicAuthenticationProvider(Func<Task<string>> credentials)
+        {
+            this._credentials = credentials;
+        }
+
+        /// <summary>
+        /// Applies the authentication content to the provided HTTP request message.
+        /// </summary>
+        /// <param name="request">The HTTP request message.</param>
+        public async Task AuthenticateRequestAsync(HttpRequestMessage request)
+        {
+            // Base64 encode
+            string encodedContent = Convert.ToBase64String(Encoding.UTF8.GetBytes(await this._credentials().ConfigureAwait(false)));
+            request.Headers.Authorization = new AuthenticationHeaderValue("Basic", encodedContent);
+        }
+
+        /// <summary>
+        /// Applies the authentication content to the provided HTTP request message.
+        /// </summary>
+        /// <param name="request">The HTTP request message.</param>
+        public async Task AuthenticateRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            // Base64 encode
+            string encodedContent = Convert.ToBase64String(Encoding.UTF8.GetBytes(await this._credentials().ConfigureAwait(false)));
+            request.Headers.Authorization = new AuthenticationHeaderValue("Basic", encodedContent);
+        }
+    }
+}
--- SKFunctionCalling/Program.cs
+++ SKFunctionCalling/Program.cs
@@ -1,106 +1,70 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
-using Azure.AI.OpenAI;
+﻿using System.Reflection;
 using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
-using Microsoft.SemanticKernel.Connectors.AI;
-using Microsoft.SemanticKernel.Plugins.Core;
 using Microsoft.SemanticKernel.Plugins.OpenApi;
 using FunctionCalling;
 using System.Diagnostics;
 using SKFunctionCalling;
-// This example shows how to use OpenAI's tool calling capability via the chat completions interface.
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+
 public static class SKFunctionCallingTest
 {
     static async Task Main(string[] args)
     {
         await RunAsync();
     }
-    public static async Task RunAsync()
+    private static async Task RunAsync()
     {
+        //Kernel Builder
         IKernelBuilder builder = Kernel.CreateBuilder();
-        builder.AddAzureOpenAIChatCompletion("MODEL", "ENDPOINT", "APIKEY");
+        builder.AddAzureOpenAIChatCompletion("model-name", "endpoint", "api-key");
         Kernel kernel = builder.Build();
-        var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "plugins");
         var customHandler = new CustomHttpClientHandler();
-        HttpClient azureHttpClient = new(customHandler);
-        //Generate the token from Kairo DEV. Enable Azure in plugins and get the token from the header of any http call (X-Sk-Copilot-Azure-Auth)
-        BearerAuthenticationProvider authenticationProvider = new(() => Task.FromResult(""));
+        HttpClient httpClient = new(customHandler);
+
+
+
+        //Plugins Setup
+        BearerAuthenticationProvider authenticationProvider = new(() => Task.FromResult("your-azure-token"));
         await kernel.ImportPluginFromOpenApiAsync(
         pluginName: "AzurePlugin",
         filePath: Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "plugins", "AzurePlugin/openapi.json"),
         new OpenApiFunctionExecutionParameters
         {
             AuthCallback = authenticationProvider.AuthenticateRequestAsync,
-            HttpClient = azureHttpClient
+            HttpClient = httpClient
         });
 
+        var authenticationProvider1 = new BasicAuthenticationProvider(() => { return Task.FromResult("your-jira-token"); });
+        await kernel.ImportPluginFromOpenApiAsync(
+            pluginName: "JiraPlugin",
+            filePath: Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "plugins", "JiraPlugin/openapi.json"),
+            new OpenApiFunctionExecutionParameters
+            {
+                AuthCallback = authenticationProvider1.AuthenticateRequestAsync,
+                ServerUrlOverride = new Uri("https://kmeleontech.atlassian.net/rest/api/latest/"),
+                HttpClient = httpClient,
+            });
 
-        Stopwatch stopwatch = Stopwatch.StartNew();
+
+
         Console.WriteLine("======== Example 1: Use automated function calling with a non-streaming prompt ========");
         {
-            OpenAIPromptExecutionSettings settings = new() { ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions };
-            Console.WriteLine(await kernel.InvokePromptAsync("RESTART virtual machine with name vm-kairo-dev-001 for subscriptionId 'fb3a669d-9793-49f3-9607-3881a804cff6', resource group rg-genresources-dev-eastus and api-version 2023-03-1. DON´t forget to add api-version as a query parameter", new(settings)));
+            OpenAIPromptExecutionSettings settings = new() { ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions,
+                                                     ChatSystemPrompt = "This assistant will provide the user with both Azure and Jira operations" };
+            // Get user input for the prompt
+            Console.Write("Enter the prompt: ");
+            string userPrompt = Console.ReadLine();
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            // Invoke the prompt with user input
+            Console.WriteLine(await kernel.InvokePromptAsync(userPrompt, new(settings)));
             Console.WriteLine();
             // Stop the stopwatch.
             stopwatch.Stop();
-
             // Write the time taken to execute the code.
-            Console.WriteLine($"Time taken: {stopwatch.ElapsedMilliseconds} milliseconds");
+            Console.WriteLine($"Time taken: {stopwatch.ElapsedMilliseconds} milliseconds");        
         }
-
-
-
-        Stopwatch stopwatch1 = Stopwatch.StartNew();
-        Console.WriteLine("======== Example 3: Use manual function calling with a non-streaming prompt ========");
-        {
-            var chat = kernel.GetRequiredService<IChatCompletionService>();
-            var chatHistory = new ChatHistory();
-
-            OpenAIPromptExecutionSettings settings = new() { ToolCallBehavior = ToolCallBehavior.EnableKernelFunctions };
-            chatHistory.AddUserMessage("Restart virtual machine vm-kairo-dev-001 for subscriptionId 'fb3a669d-9793-49f3-9607-3881a804cff6', resource group rg-genresources-dev-eastus and api version 2023-03-1");
-            while (true)
-            {
-                var result = (OpenAIChatMessageContent)await chat.GetChatMessageContentAsync(chatHistory, settings, kernel);
-
-                if (result.Content is not null)
-                {
-                    Console.Write(result.Content);
-                }
-
-                List<ChatCompletionsFunctionToolCall> toolCalls = result.ToolCalls.OfType<ChatCompletionsFunctionToolCall>().ToList();
-                if (toolCalls.Count == 0)
-                {
-                    break;
-                }
-
-                chatHistory.Add(result);
-                foreach (var toolCall in toolCalls)
-                {
-                    string content = kernel.Plugins.TryGetFunctionAndArguments(toolCall, out KernelFunction? function, out KernelArguments? arguments) ?
-                        JsonSerializer.Serialize((await function.InvokeAsync(kernel, arguments)).GetValue<object>()) :
-                        "Unable to find function. Please try again!";
-
-                    chatHistory.Add(new ChatMessageContent(
-                        AuthorRole.Tool,
-                        content,
-                        metadata: new Dictionary<string, object?>(1) { { OpenAIChatMessageContent.ToolIdProperty, toolCall.Id } }));
-                }
-            }
-
-            Console.WriteLine();
-        }
-        stopwatch1.Stop();
-
-        // Write the time taken to execute the code.
-        Console.WriteLine($"Time taken: {stopwatch.ElapsedMilliseconds} milliseconds");
     }
-}// See https://aka.ms/new-console-template for more information
+}
 
